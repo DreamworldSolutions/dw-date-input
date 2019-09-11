9b06f679641b6bcc39f979c3c13a63be242caa87
ruchita kheni <ruchita.kheni@dreamworldsolutions.com>
2019-09-11 17:17:54 +0530

    fix: use icon property of dw-input

--- dw-date-input.js
+++ dw-date-input.js
@@ -12,8 +12,9 @@ import { LitElement, html, css } from 'lit-element';
 import '@dreamworld/dw-input/dw-input';
 import { DwFormElement } from '@dreamworld/dw-form/dw-form-element';
 import moment from 'moment';
-import '@vaadin/vaadin-date-picker';
+import '@vaadin/vaadin-date-picker/theme/material/vaadin-date-picker';
 import './vaadin-text-field-style';
+import './date-picker-overlay-style';
   
 export class DwDateInput extends DwFormElement(LitElement) {
   static get styles() {
@@ -182,7 +183,7 @@ export class DwDateInput extends DwFormElement(LitElement) {
        .errorMessage="${this._getErrorMessage(this.value, this.errorMessagesByState, this._errorState)}"
        .name="${this.name}"
        .hint="${this.hint}"
-       .sufixSvgIcon="${this._getCalenderIcon()}"
+       iconTrailing="date_range"
        hasClickableIcon
        @blur="${this._onBlur}"
        @click="${this._onClick}"
@@ -255,7 +256,7 @@ export class DwDateInput extends DwFormElement(LitElement) {
 
   layout() { 
     let el = this.shadowRoot.querySelector('dw-input');
-    el.layout();
+    el && el.layout();
   }
 
   /**
@@ -450,17 +451,15 @@ export class DwDateInput extends DwFormElement(LitElement) {
   }
 
   /**
-   * Returns calender icon as a string
-   */
-  _getCalenderIcon() { 
-    return '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M9 11H7v2h2v-2zm4 0h-2v2h2v-2zm4 0h-2v2h2v-2zm2-7h-1V2h-2v2H8V2H6v2H5c-1.11 0-1.99.9-1.99 2L3 20c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V9h14v11z"/><path fill="none" d="M0 0h24v24H0z"/></svg>';
-  }
-
-  /**
    * open date picker
    */
   _onClick(e) {
-    if ((e.composedPath()[0].tagName === 'svg' || e.composedPath()[0].tagName === 'path' ) && !this.disabled && !this.readOnly) { 
+    let paths = e.composedPath()
+    let iconIndex = paths.findIndex((path) => {
+      return path.tagName === 'DW-ICON';
+    });
+
+    if (iconIndex !== -1 && !this.disabled && !this.readOnly) { 
       this.shadowRoot.querySelector('vaadin-date-picker').opened = true;
       this.focus();
     }
@@ -473,6 +472,7 @@ export class DwDateInput extends DwFormElement(LitElement) {
     if (!e.detail.value) { 
       return;
     }
+    
     let selectedDate = moment(e.detail.value, 'YYYY-MM-DD').format(this.inputFormat.toUpperCase());
     this.value = selectedDate;
 
