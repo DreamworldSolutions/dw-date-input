1b546140ec2bf551ef22c209ae0c4dbad6a1fee2
Hiten Bhayani <hiten@hisab.co>
2023-02-22 14:19:10 +0530

    feat: create `dw-month-year-grid` to select year wise month

--- demo/dw-date-input-demo.js
+++ demo/dw-date-input-demo.js
@@ -3,8 +3,22 @@ import { LitElement, html, css } from "@dreamworld/pwa-helpers/lit.js";
 import "../dw-date-input.js";
 import "../dw-date-range-selection.js";
 import "../date-input.js";
+import "../dw-month-year-grid.js";
+
+import {ThemeStyle} from "@dreamworld/material-styles/theme.js";
 
 export class DwDateInputDemo extends LitElement {
+
+  static get styles() {
+    return [
+      ThemeStyle,
+      css`
+        dw-month-year-grid {
+          max-height: 500px;
+        }
+      ` 
+    ]
+  }
 
   static get properties() {
     return {
@@ -25,6 +39,9 @@ export class DwDateInputDemo extends LitElement {
 
       <h4>Required</h4>
       <dw-date-input label="Start date" placeholder="Enter date here" showFutureWarning ></dw-date-input>
+
+      <br>
+      <dw-month-year-grid @value-changed=${this._onMonthSelect}></dw-month-year-grid>
 
       <!-- <h4>Required</h4>
       <dw-date-input label="Start date" placeholder="Enter date here" required errorMessage="Required"></dw-date-input>
@@ -69,6 +86,10 @@ export class DwDateInputDemo extends LitElement {
   _onValueChange(e) {
     console.log(e);
     this.value = e.detail.value;
+  }
+
+  _onMonthSelect(e) {
+    console.log(e)
   }
 }
 
