6be8bd4e819d7482ee2b33f077a6d4af7106e40f
Shakil Mansuri <shakil.mansuri@dreamworldsolutions.com>
2024-03-20 13:52:23 +0530

    fix: two date-picker opens simentensoly

--- dw-date-input.js
+++ dw-date-input.js
@@ -348,46 +348,44 @@ export class DwDateInput extends DwFormElement(LitElement) {
 
   get dateInputTemplate() {
     return html`
-      <div class="date-input">
-        <date-input
-          id="dateInput"
-          .iconTrailing=${'date_range'}
-          .clickableIcon=${true}
-          .inputFormat="${this.inputFormat}"
-          .valueFormat=${this.valueFormat}
-          .label="${this.label}"
-          ?disabled="${this.disabled}"
-          .invalid=${this.invalid}
-          ?noLabel="${this.noLabel}"
-          ?required="${this.required}"
-          ?readOnly="${this.readOnly || this.mobileMode || this.tabletMode}"
-          ?autoSelect="${this.autoSelect}"
-          ?dense="${this.dense}"
-          ?hintPersistent="${this.hintPersistent}"
-          .placeholder="${this.placeholder}"
-          ?highlightChanged="${this.highlightChanged}"
-          ?noHintWrap="${this.noHintWrap}"
-          .date="${this.value}"
-          .originalDate="${this.originalValue}"
-          .name="${this.name}"
-          .hint="${this.hint}"
-          .minDate="${this.minDate}"
-          .maxDate="${this.maxDate}"
-          .showFutureWarning=${this.showFutureWarning}
-          .showFutureError=${this.showFutureError}
-          .warning=${this._warning}
-          .error=${this._error}
-          .hintInTooltip="${this.hintInTooltip}"
-          .errorInTooltip="${this.errorInTooltip}"
-          .warningInTooltip="${this.warningInTooltip}"
-          .hintTooltipActions="${this.hintTooltipActions}"
-          .errorTooltipActions="${this.errorTooltipActions}"
-          .warningTooltipActions="${this.warningTooltipActions}"
-          .tipPlacement="${this.tipPlacement}"
-          .errorMessages="${this._errorMessages}"
-          @change=${this._onChange}
-        ></date-input>
-      </div>
+      <date-input
+        id="dateInput"
+        .iconTrailing=${'date_range'}
+        .clickableIcon=${true}
+        .inputFormat="${this.inputFormat}"
+        .valueFormat=${this.valueFormat}
+        .label="${this.label}"
+        ?disabled="${this.disabled}"
+        .invalid=${this.invalid}
+        ?noLabel="${this.noLabel}"
+        ?required="${this.required}"
+        ?readOnly="${this.readOnly || this.mobileMode || this.tabletMode}"
+        ?autoSelect="${this.autoSelect}"
+        ?dense="${this.dense}"
+        ?hintPersistent="${this.hintPersistent}"
+        .placeholder="${this.placeholder}"
+        ?highlightChanged="${this.highlightChanged}"
+        ?noHintWrap="${this.noHintWrap}"
+        .date="${this.value}"
+        .originalDate="${this.originalValue}"
+        .name="${this.name}"
+        .hint="${this.hint}"
+        .minDate="${this.minDate}"
+        .maxDate="${this.maxDate}"
+        .showFutureWarning=${this.showFutureWarning}
+        .showFutureError=${this.showFutureError}
+        .warning=${this._warning}
+        .error=${this._error}
+        .hintInTooltip="${this.hintInTooltip}"
+        .errorInTooltip="${this.errorInTooltip}"
+        .warningInTooltip="${this.warningInTooltip}"
+        .hintTooltipActions="${this.hintTooltipActions}"
+        .errorTooltipActions="${this.errorTooltipActions}"
+        .warningTooltipActions="${this.warningTooltipActions}"
+        .tipPlacement="${this.tipPlacement}"
+        .errorMessages="${this._errorMessages}"
+        @change=${this._onChange}
+      ></date-input>
     `;
   }
 
@@ -456,7 +454,6 @@ export class DwDateInput extends DwFormElement(LitElement) {
         .inputFormat=${this.inputFormat}
         .valueFormat=${this.valueFormat}
         .triggerElement=${this.triggerElement}
-        .excludeOutsideClickFor=${'date-input'}
         @dw-dialog-closed=${(e) => this._triggerDatePickerOpenedChanged(false)}
         @dw-dialog-opened=${(e) => this._triggerDatePickerOpenedChanged(true)}
         @mode-changed=${this._onDatePickerModeChanged}
