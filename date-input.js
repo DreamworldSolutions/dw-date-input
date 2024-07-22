b65babf00c5ed0a6d94641d7d3fe28fad986e848
Ujjval Kukadiya <ujjval@dreamworld.dev>
2024-07-22 14:41:48 +0530

    fix:  usability issues & enhancements.

--- date-input.js
+++ date-input.js
@@ -23,10 +23,6 @@ export class DateInput extends DwInput {
           pointer-events: none;
         }
 
-        .mdc-text-field--focused:not(.mdc-text-field--invalid):not(.mdc-text-field--disabled) {
-          --dw-icon-color: var(--mdc-theme-primary);
-        }
-
         .mdc-text-field--with-leading-icon.mdc-text-field--dense .mdc-text-field__icon, 
         .mdc-text-field--with-trailing-icon.mdc-text-field--dense .mdc-text-field__icon {
           transform: scale(1);
@@ -36,6 +32,11 @@ export class DateInput extends DwInput {
 
         .mdc-text-field--outlined .mdc-text-field__input {
           padding: var(--dw-date-input-padding, 12px 16px 14px);
+        }
+
+        .mdc-text-field__icon:not([tabindex]),
+        .mdc-text-field__icon[tabindex='-1'] {
+          pointer-events: auto;
         }
       `
     ]
