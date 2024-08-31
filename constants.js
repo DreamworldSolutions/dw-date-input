2e99417dc2ac3afc47756a4482cb1afe0fe3da17
Ujjval Kukadiya <ujjval@dreamworld.dev>
2024-08-31 15:34:57 +0530

    fix: add new constants.

---
+++ constants.js
@@ -0,0 +1,10 @@
+export const currentYearFormat = {
+    'DD-MM-YYYY': 'DD-MM',
+    'DD/MM/YYYY': 'DD/MM',
+    'MM/DD/YYYY': 'MM/DD',
+    'MM-DD-YYYY': 'MM-DD',
+    'YYYY/MM/DD': 'MM/DD',
+    'YYYY-MM-DD': 'MM-DD',
+    'DD MMM YYYY': 'DD MMM',
+    'MMM DD, YYYY': 'MMM DD',
+  };
